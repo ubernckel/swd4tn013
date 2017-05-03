71d6744a99f4c58f5a5246fa7fb12445e94a17c3
ubernckel <ubernickel@outlook.com>
2017-05-03 17:36:51 +0300

    add mysql test

---
+++ mysli/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Müesli</title>
+</head>
+<body>
+
+
+<html>
+<head>
+<title>
+Müesli
+</title>
+</head>
+<body>
+<h1>Müesli</h1>
+
+<?php
+print('moe');
+
+
+?>
+
+</body>
+</html>
+
+
+
